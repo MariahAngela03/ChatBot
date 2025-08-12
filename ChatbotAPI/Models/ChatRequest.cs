fe8f0895a84967bee8e7e9b82442de233ee9778e
MariahAngela <saulmariahangelaa@gmail.com>
2025-08-12 12:06:46 +0800

    ulit

--- ChatbotAPI/Models/ChatRequest.cs
+++
@@ -1,9 +0,0 @@
-namespace ChatbotAPI.Models
-{
-    public class ChatRequest
-    {
-        public string Message { get; set; } = string.Empty;
-        public string? SessionId { get; set; }
-        public Dictionary<string, object>? Context { get; set; }
-    }
-}
